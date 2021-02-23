bc45daf33bb8616a58e94ebeced0613f391f63bf
bumbum9944 <tong940526@gmail.com>
2021-02-23 15:59:31 +0900

    project start

---
+++ backend/account.sql
@@ -0,0 +1,7 @@
+create table if not exists user(
+    id int not null AUTO_INCREMENT,
+    name varchar(50) not null,
+    name varchar(100) not null,
+    name varchar(100) not null,
+    primary key (id)
+);
---
+++ backend/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse
+import pymysql
+
+
+app = Flask(__name__)
+api = Api(app)
+
+# db 연결
+db = pymysql.connect(
+    user = 'root',
+    passwd = '',
+    host = '127.0.0.1',
+    port = 3306,
+    db = 'elice_pjt1',
+    charset = 'utf8'
+)
+
+cursor = db.cursor()
+
+parser = reqparse.RequestParser()
+parser.add_argument("email")
+parser.add_argument("password")
+parser.add_argument("name")
+
+test_db = dict()
+
+class Account(Resource):
+    def post(self):
+        args = parser.parse_args()
+        # 이름이 없으면 로그인
+        if args['name'] == None:
+            sql = "SELECT email, password FROM user WHERE email=%s;"
+
+            cursor.execute(sql, (args['email'], ))
+            result = cursor.fetchone()
+
+            # 유저가 존재하지 않을 경우
+            if result == None:
+                return jsonify(status="success", result="존재하지 않는 유저입니다.")
+            else:
+                # 유저가 있다면 비밀번호 체크
+                if args['password'] == result[1]:
+                    return jsonify(status="success", result="로그인 성공!!")
+                else:
+                    return jsonify(status="success", result="비밀번호를 확인해주세요")
+        else:
+            sql = """
+            INSERT INTO user (name, email, password) 
+            VALUES (%s, %s, %s);
+            """
+            cursor.execute(sql, (args['name'], args['email'], args['password'], ))
+            db.commit()
+
+            sql = "SELECT email FROM user WHERE email=%s;"
+            cursor.execute(sql, (args['email'], ))
+            result = cursor.fetchone()
+            
+            return jsonify(status = "success", result = result) 
+
+# api 라우팅 등록
+api.add_resource(Account, '/account')
+
+if __name__ == '__main__':
+    app.run(debug=True)
