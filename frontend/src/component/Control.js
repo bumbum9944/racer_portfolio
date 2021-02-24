e080c88c9554d43edba2fc7f566678deb4ce3531
bumbum9944 <tong940526@gmail.com>
2021-02-24 15:39:23 +0900

    로그아웃 기능 구현 - 세션스토리지 토큰 삭제

--- frontend/src/component/Control.js
+++ frontend/src/component/Control.js
@@ -1,28 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 function Control(props) {
   return (
-    <ul>
-      <li>
-        <input
-          type="button"
-          value="login"
-          onClick={function() {
-            props.onChangeMode("LOGIN");
-          }}
-        />
-      </li>
-      <li>
-        <input
-          type="button"
-          value="signUp"
-          onClick={function() {
-            props.onChangeMode("SIGNUP");
-          }}
-        />
-      </li>
-    </ul>
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/login">로그인</Link>
+      <Link to="/signup">회원가입</Link>
+      <button onClick={
+        function() {
+          sessionStorage.removeItem('token');
+          props.history.push('/login');
+        }
+      }>로그아웃</button>
+    </>
   );
 }
 
-export default Control;
+export default withRouter(Control);
