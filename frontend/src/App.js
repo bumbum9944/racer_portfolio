51f12ccc4ca33434461dcd12f4acf817b827d11b
bumbum9944 <tong940526@gmail.com>
2021-02-24 15:51:49 +0900

    Control 컴포넌트를 Nav로 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,15 @@ import { React} from 'react';
 import { Route } from 'react-router-dom';
 import Login from './component/Login'
 import Signup from './component/Signup'
-import Control from './component/Control'
+import Nav from './component/Nav'
 import Home from './component/Home'
 
 
 function App() {
-  // var [mode, setMode] = useState('LOGIN');
-
-  // var formTag = '';
-  // if(mode === 'LOGIN') {
-  //   formTag = <Login />
-  // } else if(mode === 'SIGNUP') {
-  //   formTag = <Signup />
-  // }
 
   return (
     <div className="App">
-      {/* <Control onChangeMode={function(mode) {
-        setMode(mode)
-      }}/> */}
-      <Control />
+      <Nav />
       <Route path="/" component={Home} exact={true} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
